d192024d84286a4bbe3b6f78abea4983e3a21b3a
ajhughesdev <andrew@ajhughes.dev>
2026-03-29 00:13:17 -0400

    test: add seam coverage and truthful tooling

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var es = require('event-stream');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
+var sass = require('gulp-sass')(require('sass'));
+var autoprefixer = require('gulp-autoprefixer');
+var prefix = autoprefixer.default || autoprefixer;
 var sourcemaps = require('gulp-sourcemaps');
-var cssnano = require('gulp-cssnano');
-var cache = require('gulp-cache');
-var del = require('del');
-var runSequence = require('run-sequence');
 var gutil = require('gulp-util');
 
 
@@ -35,64 +26,11 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('dist/css')); // Outputs it in the css folder
 });
 
-// Compile JavaScript
-gulp.task('js', function() {
-
-    var sourcePath = 'src/js/';
-
-    var files = [
-        'resources.js',
-    ];
-
-    var tasks = files.map(function(entry) {
-        return browserify({ entries: [sourcePath + entry], debug: true })
-            .bundle()
-            .pipe(source(entry))
-            .pipe(rename({
-                extname: '.min.js'
-            }))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({loadMaps: true}))
-            // Add transformation tasks to the pipeline here.
-            .pipe(uglify())
-            .on('error', gutil.log)
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest('dist/js/'));
-    });
-
-    return es.merge.apply(null, tasks);
-});
-
 // Watchers
 gulp.task('watch', function () {
-    gulp.watch('src/js/**/*.js', gulp.series('js'));
     gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
 });
 
-// Optimization Tasks
-// ------------------
+gulp.task('build', gulp.series('sass'));
 
-// Cleaning
-gulp.task('clean', function () {
-    return del.sync('dist').then(function (cb) {
-        return cache.clearAll(cb);
-    });
-});
-
-// Build Sequences
-// ---------------
-
-gulp.task('default', function (callback) {
-    runSequence(['js', 'sass'], 'watch',
-        callback
-    )
-});
-
-gulp.task('build', function (callback) {
-    runSequence(
-        'clean:dist',
-        'js',
-        'sass',
-        callback
-    )
-});
+gulp.task('default', gulp.series('sass', 'watch'));
---
+++ tests/bootstrap.php
@@ -0,0 +1,120 @@
+<?php
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+class MenuElementsTestFailure extends \RuntimeException
+{
+}
+
+$GLOBALS['menu_elements_test_assertions'] = 0;
+
+function menu_elements_test_increment_assertions()
+{
+    $GLOBALS['menu_elements_test_assertions']++;
+}
+
+function menu_elements_test_export($value)
+{
+    return var_export($value, true);
+}
+
+function menu_elements_test_fail($message)
+{
+    throw new MenuElementsTestFailure($message);
+}
+
+function menu_elements_test_assert_same($expected, $actual, $message = '')
+{
+    menu_elements_test_increment_assertions();
+
+    if ($expected !== $actual) {
+        $details = 'Expected ' . menu_elements_test_export($expected) . ' but got ' . menu_elements_test_export($actual) . '.';
+        menu_elements_test_fail(trim($message . ' ' . $details));
+    }
+}
+
+function menu_elements_test_assert_true($condition, $message = '')
+{
+    menu_elements_test_increment_assertions();
+
+    if ($condition !== true) {
+        menu_elements_test_fail($message !== '' ? $message : 'Expected condition to be true.');
+    }
+}
+
+function menu_elements_test_assert_contains($needle, $haystack, $message = '')
+{
+    menu_elements_test_increment_assertions();
+
+    if (strpos($haystack, $needle) === false) {
+        $details = 'Did not find ' . menu_elements_test_export($needle) . ' in ' . menu_elements_test_export($haystack) . '.';
+        menu_elements_test_fail(trim($message . ' ' . $details));
+    }
+}
+
+function menu_elements_test_assert_instance_of($expectedClass, $value, $message = '')
+{
+    menu_elements_test_increment_assertions();
+
+    if (!($value instanceof $expectedClass)) {
+        $actualClass = is_object($value) ? get_class($value) : gettype($value);
+        $details = 'Expected instance of ' . $expectedClass . ' but got ' . $actualClass . '.';
+        menu_elements_test_fail(trim($message . ' ' . $details));
+    }
+}
+
+function menu_elements_test_assert_throws(callable $callback, $expectedClass, $expectedMessagePart = '')
+{
+    menu_elements_test_increment_assertions();
+
+    try {
+        $callback();
+    } catch (\Throwable $throwable) {
+        if (!($throwable instanceof $expectedClass)) {
+            menu_elements_test_fail('Expected exception ' . $expectedClass . ' but got ' . get_class($throwable) . '.');
+        }
+
+        if ($expectedMessagePart !== '' && strpos($throwable->getMessage(), $expectedMessagePart) === false) {
+            menu_elements_test_fail('Expected exception message to contain ' . menu_elements_test_export($expectedMessagePart) . ' but got ' . menu_elements_test_export($throwable->getMessage()) . '.');
+        }
+
+        return;
+    }
+
+    menu_elements_test_fail('Expected exception ' . $expectedClass . ' to be thrown.');
+}
+
+if (!function_exists('apply_filters')) {
+    function apply_filters($tag, $value)
+    {
+        return $value;
+    }
+}
+
+if (!function_exists('get_field')) {
+    function get_field($field, $item)
+    {
+        return isset($item->$field) ? $item->$field : null;
+    }
+}
+
+if (!function_exists('get_the_title')) {
+    function get_the_title($item)
+    {
+        return isset($item->title) ? $item->title : '';
+    }
+}
+
+if (!function_exists('sanitize_html_class')) {
+    function sanitize_html_class($class)
+    {
+        return preg_replace('/[^A-Za-z0-9_-]/', '', (string) $class);
+    }
+}
+
+if (!function_exists('esc_html')) {
+    function esc_html($text)
+    {
+        return htmlspecialchars((string) $text, ENT_QUOTES, 'UTF-8');
+    }
+}
